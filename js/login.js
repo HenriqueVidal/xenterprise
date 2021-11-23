a96a6cf730f2ac490a27d85ef951cea65b47e481
HenriqueVidal <hpvidal@outlook.com>
2021-11-22 23:12:33 -0300

    usuario e senha via js

---
+++ js/login.js
@@ -0,0 +1,16 @@
+
+
+
+function login(form) {
+    if (form.user.value=="henrique") { 
+            if (form.pass.value=="vidal") { 
+
+            location="./cams.html"
+            }   else {
+
+                alert("Senha Incorreta")
+            }
+    } else {
+        alert("Usuário Incorreto")
+    }
+}		
